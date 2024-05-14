8846f396daea93423b37689877aa748787cc1843
Alexander <7086600@gmail.com>
2024-05-14 05:20:33 +0300

    add goals and data description

--- code/micosts.py
+++ code/micosts.py
@@ -1,2 +1,17 @@
 # investigating a medical insurance costs dataset
 
+'''
+Goals: ideas for analysis:
+ - Find out the average age of the patients in the dataset.
+ - Analyze where a majority of the individuals are from.
+ - Look at the different costs between smokers vs. non-smokers.
+ - Figure out what the average age is for someone who has at least one child in this dataset.
+'''
+
+'''
+Data
+ - csv-file which contents the dataset with: age,sex,bmi,children,smoker,region,charges
+'''
+
+
+
